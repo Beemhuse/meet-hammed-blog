daf3d38ed7e8f2e4323dc2742f3226d0fe42213f
Beemhuse <brightawah94@gmail.come>
2025-02-01 19:35:05 +0100

    fix: contact page

--- src/app/(web)/blog/page.jsx
+++ src/app/(web)/blog/page.jsx
@@ -1,6 +1,8 @@
-"use client"
+"use client";
 import Card from "@/components/card/Card";
 import CategoryTabs from "@/components/category-tabs";
+import { Suspense } from "react";
+
 // import { client } from "@/sanity/client";
 import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
@@ -8,35 +10,38 @@ import useSWR from "swr";
 export default function BlogPage() {
   const searchParams = useSearchParams();
   const category = searchParams.get("category");
-  
-  const query = category && category !== "all"
-  ? `*[_type == "post" && categories[]->slug.current == "${category}"]`
-  : `*[_type == "post"]`;
-  
-  const {data: posts} = useSWR(query)
+
+  const query =
+    category && category !== "all"
+      ? `*[_type == "post" && categories[]->slug.current == "${category}"]`
+      : `*[_type == "post"]`;
+
+  const { data: posts } = useSWR(query);
   // const posts = await client.fetch(query);
-console.log(posts)
+  console.log(posts);
   return (
     <div className="p-6">
       <CategoryTabs />
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
-        {posts?.length > 0 ? (
-          posts?.map((post) => (
-            <Card
-            key={post._id}
-            image={post.image}
-            category={post.categories}
-            title={post.title}
-            author={post.author}
-            date={post.publishedAt}
-            slug={post.slug}
-            authorImage={post.authorImage}
-          />
-          ))
-        ) : (
-          <p className="text-gray-500">No posts found.</p>
-        )}
-      </div>
+      <Suspense>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-6">
+          {posts?.length > 0 ? (
+            posts?.map((post) => (
+              <Card
+                key={post._id}
+                image={post.image}
+                category={post.categories}
+                title={post.title}
+                author={post.author}
+                date={post.publishedAt}
+                slug={post.slug}
+                authorImage={post.authorImage}
+              />
+            ))
+          ) : (
+            <p className="text-gray-500">No posts found.</p>
+          )}
+        </div>
+      </Suspense>
     </div>
   );
 }
