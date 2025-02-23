2b6c821c16c100e15abeeda88ab0281fbdac0ef2
Beemhuse <brightawah94@gmail.come>
2025-02-23 15:45:27 +0100

    fix: changed to cloudflare worker url

--- src/app/a-dashboard/page.jsx
+++ src/app/a-dashboard/page.jsx
@@ -2,7 +2,8 @@ export default function Page() {
   return (
     <>
       <iframe
-        src="https://hammed-blog.sanity.studio/"
+        // src="https://hammed-blog.sanity.studio/"
+        src="https://holy-truth-3a24.brightawah94.workers.dev/"
         className="w-full h-screen"
       />{" "}
     </>
